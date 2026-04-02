becc420d25f08bffe8c4c7c9282adb7f289432b5
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-02 12:14:15 +0800

    membuat fungsi mergeSort dan tentukan titik tengah (mid)

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -32,5 +32,13 @@ cout << "\n---" << endl;
         cin >> arr[i];
     }
 }
-     
-   
+
+// create function mergeSort
+void mergeSort(int low, int high)
+{
+     if (low >= high) // step 1
+        return;      // step 1.a
+
+    int mid = (low + high) / 2; // step 2
+
+}
